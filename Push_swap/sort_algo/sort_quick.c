15b4fccd7027d1c1f8d5e4623b8c57d001100cf7
Emile ORER <eorer@e2r9p8.clusters.42paris.fr>
2023-02-15 14:49:06 +0100

    Change pushswap

--- Push_swap/sort_algo/sort_quick.c
+++ Push_swap/sort_algo/sort_quick.c
@@ -5,77 +5,127 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: eorer <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/09 16:54:26 by eorer             #+#    #+#             */
-/*   Updated: 2022/12/12 15:13:57 by eorer            ###   ########.fr       */
+/*   Created: 2022/12/14 14:37:06 by eorer             #+#    #+#             */
+/*   Updated: 2022/12/20 12:31:09 by eorer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
-#include <stdio.h>
 
-static	void	ft_swap(int *a, int *b)
+int	partition(t_pile *a, t_pile *b, int size)
 {
-	int	tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-void	printf_array(int *arr, int size)
-{
+	int	push;
 	int	i;
+	int	pivot;
+	int	*srt_lst;
 
+	srt_lst = sorted_tab(a, size);
+	pivot = srt_lst[size / 2];
 	i = 0;
+	push = 0;
 	while (i < size)
 	{
-		printf("%i ", arr[i]);
+		if (a->content[0] < pivot)
+		{
+			push_b(a, b);
+			push++;
+		}
+		else
+			rotate_a(a, b, 0);
 		i++;
 	}
-	printf("\n");
+	free(srt_lst);
+	return (push);
 }
 
-static	int	partition(int arr[], int low, int high)
+int	partition_bis(t_pile *a, t_pile *b, int size)
 {
-	int	pivot;
+	int	push;
 	int	i;
-	int	j;
+	int	pivot;
+	int	*srt_lst;
 
-	pivot = arr[high];
-	i = low - 1;
-	j = low;
-	while (j <= high - 1)
+	srt_lst = sorted_tab(a, size);
+	pivot = srt_lst[size / 2];
+	i = 0;
+	push = 0;
+	while (i < size)
 	{
-		if (arr[j] <= pivot)
+		if (a->content[0] < pivot)
 		{
-			i++;
-			ft_swap(&arr[i], &arr[j]);
+			push_b(a, b);
+			push++;
 		}
-		j++;
+		else
+			rotate_a(a, b, 0);
+		i++;
 	}
-	ft_swap(&arr[i + 1], &arr[high]);
-	return (i + 1);
+	i = -1;
+	while (i++ < size - push - 1)
+		reverse_rotate_a(a, b, 0);
+	free(srt_lst);
+	return (push);
 }
 
-void	quicksort(int *arr, int low, int high)
+void	recursive_bis(t_pile *a, t_pile *b, int size)
 {
-	int	sep;
+	int	push;
+	int	i;
 
-	if (low < high)
+	i = -1;
+	if (size <= 3)
 	{
-		sep = partition(arr, low, high);
-		quicksort(arr, low, sep - 1);
-		quicksort(arr, sep + 1, high);
+		three_digit_sort(a, b);
+		return ;
+	}
+	push = partition_bis(a, b, size);
+	recursive_bis(a, b, size - push);
+	if (push <= 3)
+	{
+		while (i++ < push - 1)
+			push_a(a, b);
+		three_digit_sort(a, b);
+		recursive_bis(a, b, push);
+	}
+	else
+	{
+		while (i++ < push - 1)
+			push_a(a, b);
+		recursive_bis(a, b, push);
 	}
 }
 
-/*int	main()
+void	recursive(t_pile *a, t_pile *b, int size)
 {
-	int	array[] = {4, 1, 7, 3, 6, 13, 2, 5, 11, 0, 7};
-	int	size = sizeof(array)/sizeof(array[0]);
+	int	push;
+	int	i;
 
-	printf_array(array, size);
-	quicksort(array, 0, size - 1);
-	printf_array(array, size);
-	return(0);
-}*/
+	i = -1;
+	if (size <= 3)
+	{
+		three_digit_sort(a, b);
+		return ;
+	}
+	push = partition(a, b, size);
+	recursive(a, b, size - push);
+	if (push <= 3)
+	{
+		while (i++ < push - 1)
+			push_a(a, b);
+		three_digit_sort(a, b);
+		recursive_bis(a, b, push);
+	}
+	else
+	{
+		while (i++ < push - 1)
+			push_a(a, b);
+		recursive_bis(a, b, push);
+	}
+}
+
+void	sort_quick(t_pile *a, t_pile *b, int size)
+{
+	if (is_sorted(a->content, size))
+		return ;
+	recursive(a, b, a->size);
+}
