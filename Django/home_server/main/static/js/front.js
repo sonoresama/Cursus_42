b84c97da577b7080af8acf2f3c2724e2b51a2bcc
Emile ORER <eorer@bess-f2r1s3.clusters.42paris.fr>
2024-03-12 19:02:02 +0100

    Django home server -- download works

---
+++ Django/home_server/main/static/js/front.js
@@ -0,0 +1,8 @@
+function toggle_list(id){
+  var container = document.getElementById(id);
+  container.classList.toggle("active");
+}
+
+function download(){
+  console.log('download');
+}
--- Django/home_server/main/urls.py
+++ Django/home_server/main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
-    path("", views.index, name="index")
-}
+urlpatterns = [
+    path("<str:directory>/download_zip", views.download_zip, name="download_zip"),
+    path("", views.index, name="index"),
+]
